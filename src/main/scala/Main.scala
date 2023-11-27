05f2b859a18a6f3b2d9a965197bc9fbe40caf617
Masatoshi Kimura <mkimura@Masatoshis-MacBook-Air.local>
2023-11-27 22:39:57 +0900

    create Deck

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,21 +1,7 @@
 import game.card.card_list.follower.Goblin
 import game.card.card_list.spell.AngelicSnipe
-import game.card.{Card, FollowerCard, SpellCard}
+import game.card.{Card, Deck, FollowerCard, SpellCard}
 
 object Main extends App:
-  def f(c: Card) =
-    c match
-      case f: FollowerCard =>
-        println(s"followerCard: ${f}")
-      case s: SpellCard =>
-        println(s"followerCard: ${s}")
-
-  val goblin = Goblin()
-  val angelicSnipe = AngelicSnipe()
-
-
-  println(s"goblin: ${goblin.cost}")
-  println(s"angelicSnipe: ${angelicSnipe.cost}")
-  f(goblin)
-  f(angelicSnipe)
-
+  val deck = Deck.initDeck()
+  println(s"deck: $deck")
--- src/main/scala/game/card/Deck.scala
+++ src/main/scala/game/card/Deck.scala
@@ -1,3 +1,14 @@
 package game.card
 
-case class Deck()
+import game.card.card_list.follower.Goblin
+import game.card.card_list.spell.AngelicSnipe
+
+import scala.util.Random
+
+case class Deck(cards: List[Card]):
+  def shuffle(): Deck = Deck(Random.shuffle(cards))
+object Deck:
+  def initDeck(): Deck =
+    Deck(List.fill(15)(Goblin()) ++ List.fill(15)(AngelicSnipe())).shuffle()
+
+
